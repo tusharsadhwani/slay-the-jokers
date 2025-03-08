c2f203a7caceb07efba54062c6b707372c2a5943
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2025-03-09 05:01:08 +0530

    add `--with` flags to add dependencies

--- stj.ps1
+++ stj.ps1
@@ -1,4 +1,4 @@
 if (-Not (Test-Path "$env:APPDATA\Balatro\Mods\SlayTheJokers\uvx.exe")) {
   powershell -ExecutionPolicy ByPass -c {$env:UV_UNMANAGED_INSTALL = "$env:APPDATA\Balatro\Mods\SlayTheJokers";irm https://astral.sh/uv/install.ps1 | iex}
 }
-powershell "$env:APPDATA\Balatro\Mods\SlayTheJokers\uvx.exe python@3.12 $env:APPDATA\Balatro\Mods\SlayTheJokers\stj_uploader.py"
+powershell "$env:APPDATA\Balatro\Mods\SlayTheJokers\uvx.exe --with psutil --with google-cloud-storage python@3.12 $env:APPDATA\Balatro\Mods\SlayTheJokers\stj_uploader.py"
