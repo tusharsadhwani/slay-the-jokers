2b8c174fa6924c03636bd7ec7b34e8e35ce81d82
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2025-03-10 02:06:05 +0530

    fix escaping problems

--- install_uv.ps1
+++ install_uv.ps1
@@ -1,4 +1,4 @@
 if (-Not (Test-Path "$env:APPDATA\Balatro\Mods\SlayTheJokers\uvx.exe")) {
   powershell -ExecutionPolicy ByPass -c {$env:UV_UNMANAGED_INSTALL = "$env:APPDATA\Balatro\Mods\SlayTheJokers";irm https://astral.sh/uv/install.ps1 | iex}
 }
-powershell -c "$env:APPDATA\Balatro\Mods\SlayTheJokers\uvx.exe --with psutil --with google-cloud-storage python@3.12 -c 'import psutil, google.cloud; print(`"Done!`")'"
+powershell -c "$env:APPDATA\Balatro\Mods\SlayTheJokers\uvx.exe --with psutil --with google-cloud-storage python@3.12 -c 'import psutil, google.cloud; print(chr(68), chr(111), chr(110), chr(101), chr(33))'"
