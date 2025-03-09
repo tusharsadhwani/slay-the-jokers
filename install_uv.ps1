d7e779d91614bcb73b46ce05afd1c1c2a2fe29f2
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2025-03-09 05:47:27 +0530

    install dependencies during uv install

--- install_uv.ps1
+++ install_uv.ps1
@@ -1,3 +1,4 @@
 if (-Not (Test-Path "$env:APPDATA\Balatro\Mods\SlayTheJokers\uvx.exe")) {
   powershell -ExecutionPolicy ByPass -c {$env:UV_UNMANAGED_INSTALL = "$env:APPDATA\Balatro\Mods\SlayTheJokers";irm https://astral.sh/uv/install.ps1 | iex}
 }
+powershell -c "$env:APPDATA\Balatro\Mods\SlayTheJokers\uvx.exe --with psutil --with google-cloud-storage python@3.12 -c 'import psutil, google.cloud; print(`"Done!`")'"
