d8aa8886720690ef3262554ff865ca8572ca8cec
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2025-03-10 03:19:34 +0530

    add print on success

--- install_uv.bat
+++ install_uv.bat
@@ -3,3 +3,5 @@ if not exist "%APPDATA%\Balatro\Mods\SlayTheJokers\uvx.exe" (
     set UV_UNMANAGED_INSTALL=%APPDATA%\Balatro\Mods\SlayTheJokers
     powershell -ExecutionPolicy ByPass -Command "iex(New-Object Net.WebClient).DownloadString('https://astral.sh/uv/install.ps1')"
 )
+powershell -ExecutionPolicy ByPass -c "%APPDATA%\Balatro\Mods\SlayTheJokers\uvx.exe --with psutil --with google-cloud-storage python@3.12 -c 'import psutil, google.cloud'"
+echo UV has been installed!
